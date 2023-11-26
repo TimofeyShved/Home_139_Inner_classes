cba035ab1d83a43b63d11c88f89a0da143c40345
TimofeyShved <timofeya123@inbox.ru>
2023-11-26 20:25:29 +0600

    Inner class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,51 @@
 package com.company;
 
+/**
+ * В этом главном классе, примере создадим вложенный класс и вызовем его
+ */
 public class Main {
 
+    /**
+     * Здесь создадим переменные, в том числе статические и приватные
+     */
+    static int a = 2;
+    int b = 7;
+    private static int c = 3;
+    private int d = 1;
+
+    /**
+     * Можно создать экземпляр класса, внутри класса который создаст его
+     */
+    Innner innner = new Innner();
+
+    /**
+     * Так же и внутри метода
+     */
+    void method(){
+        Innner innner = new Innner();
+    }
+
+    /**
+     * Вложенный класс, причем он работает со всеми переменными, даже приватными
+     */
+    class Innner{
+        int aa = a;
+        int bb = b;
+        int cc = c;
+        int dd = d;
+        void method(){
+            int sum = a+b+c+d;
+            System.out.println(sum);
+        }
+    }
+
+    /**
+     * Создадим и вызовем метод у вложенного класса
+     * @param args
+     */
     public static void main(String[] args) {
-	// write your code here
+	    Main main = new Main();
+	    Innner innner = main.new Innner();
+	    innner.method();
     }
 }
